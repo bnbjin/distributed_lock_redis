d20671f486d4686c78e1bdd32554b1f7b471d2e6
michaelhjy@sinonet.io <michaelhjy@sinonet.io>
2019-07-29 15:27:32 +0800

    add simple impl

---
+++ simple.py
@@ -0,0 +1,34 @@
+import redis
+
+def acquire_lock(conn, lockname, acquire_timeout=10):
+    identifier = str(uuid.uuid64())
+
+    end = time.time() + acquire_timeout
+    while time.time() < end:
+        if conn.setnx('lock:' + lockname, identifier):
+            return identifier
+
+        time.sleep(.001)
+
+    return False
+
+def release_lock(conn, lockname, identifier):
+    pipe = conn.pipeline(True)
+    lockname = 'lock:' + lockname
+
+    while True:
+        try:
+            pipe.watch(lockname)
+            if pipe.get(lockname) == identifier:
+                pipe.multi()
+                pipe.delete(lockname)
+                pipe.execute()
+                return True
+
+            pipe.unwatch()
+            break
+
+        except redis.exceptions.WatchError:
+            pass
+
+    return False
